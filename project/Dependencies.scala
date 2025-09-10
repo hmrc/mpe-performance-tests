73922a2959f34c3c6c5a88fc8b018fdeb4cce082
prode13 <190367850+prode13@users.noreply.github.com>
2025-09-10 11:41:55 +0100

    Updated the performance scripts

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,6 @@ import sbt._
 object Dependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "performance-test-runner"   % "6.2.0"
+    "uk.gov.hmrc" %% "performance-test-runner"   % "6.1.0"
   )
 }
