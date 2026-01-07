4b06758517d55e7736a1e28a0377b5af18fa8bb4
prode13 <190367850+prode13@users.noreply.github.com>
2026-01-07 12:23:20 +0000

    Performance test scripts updated to Scala 3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,6 +3,9 @@ import sbt._
 object Dependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "performance-test-runner"   % "6.3.0"
+    "org.playframework" %% "play-ahc-ws-standalone" % "3.0.10",
+    "uk.gov.hmrc" %% "performance-test-runner"   % "6.3.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.20.1",
+    "org.playframework" %% "play-ws-standalone-json" % "3.0.10"
   )
 }
--- src/test/scala/uk/gov/hmrc/perftests/mpe/MPERequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/mpe/MPERequests.scala
@@ -21,8 +21,8 @@ import io.gatling.core.check.CheckBuilder
 import io.gatling.http.Predef._
 import io.gatling.http.request.builder.HttpRequestBuilder
 import uk.gov.hmrc.performance.conf.{HttpConfiguration, ServicesConfiguration}
-import io.gatling.core.check.css.CssCheckType
-import jodd.lagarto.dom.NodeSelector
+import io.gatling.core.check.regex.RegexCheckType
+import io.gatling.core.session.Expression
 
 import scala.util.Random
 
@@ -59,7 +59,9 @@ object MPERequests extends HttpConfiguration with ServicesConfiguration {
 
   val CsrfPattern = """<input type="hidden" name="csrfToken" value="([^"]+)""""
 
-  def saveCsrfToken: CheckBuilder[CssCheckType, NodeSelector] = css("input[name='csrfToken']", "value").optional.saveAs("csrfToken")
+  def saveCsrfToken(): CheckBuilder[RegexCheckType, String] = regex(_ => CsrfPattern).saveAs("csrfToken")
+
+  val csrfToken: Expression[String] = "#{csrfToken}"
 
   def generateNino(prefix: String = "AA"): String = {
     val num = Random.nextInt(1000000)
@@ -87,86 +89,85 @@ object MPERequests extends HttpConfiguration with ServicesConfiguration {
     http("get Login Details")
       .get(loginUrl)
       .check(status.is(200))
-      .check(saveCsrfToken)
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
   }
 
   def postLogin: HttpRequestBuilder = {
     http("Post Login Details")
       .post(loginUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("authorityId", "")
-      .formParam("gatewayToken", "")
-      .formParam("redirectionUrl", startPageUrl)
-      .formParam("credentialStrength", "strong")
-      .formParam("confidenceLevel", "50")
-      .formParam("affinityGroup", "Agent")
-      .formParam("usersName", "")
-      .formParam("email", "user@test.com")
-      .formParam("credentialRole", "User")
-      .formParam("nino", "")
-      .formParam("groupIdentifier", "")
-      .formParam("agent.agentId", "")
-      .formParam("agent.agentCode", "")
-      .formParam("agent.agentFriendlyName", "")
-      .formParam("unreadMessageCount", "")
-      .formParam("mdtp.sessionId", "")
-      .formParam("mdtp.deviceId", "")
-      .formParam("presets-dropdown", "SA")
-      .formParam("enrolment[0].name", "")
-      .formParam("enrolment[0].taxIdentifier[0].name", "")
-      .formParam("enrolment[0].taxIdentifier[0].value", "")
-      .formParam("enrolment[0].state", "Activated")
-      .formParam("enrolment[1].name", "")
-      .formParam("enrolment[1].taxIdentifier[0].name", "")
-      .formParam("enrolment[1].taxIdentifier[0].value", "")
-      .formParam("enrolment[1].state", "Activated")
-      .formParam("enrolment[2].name", "")
-      .formParam("enrolment[2].taxIdentifier[0].name", "")
-      .formParam("enrolment[2].taxIdentifier[0].value", "")
-      .formParam("enrolment[2].state", "Activated")
-      .formParam("enrolment[3].name", "")
-      .formParam("enrolment[3].taxIdentifier[0].name", "")
-      .formParam("enrolment[3].taxIdentifier[0].value", "")
-      .formParam("enrolment[3].state", "Activated")
-      .formParam("enrolment[4].name", "HMRC-PODS-ORG")
-      .formParam("enrolment[4].taxIdentifier[0].name", "PSAID")
-      .formParam("enrolment[4].taxIdentifier[0].value", "A2100001")
-      .formParam("enrolment[4].state", "Activated")
-      .formParam("itmp.givenName", "")
-      .formParam("itmp.middleName", "")
-      .formParam("itmp.familyName", "")
-      .formParam("itmp.dateOfBirth", "")
-      .formParam("itmp.address.line1", "")
-      .formParam("itmp.address.line2", "")
-      .formParam("itmp.address.line3", "")
-      .formParam("itmp.address.line4", "")
-      .formParam("itmp.address.line5", "")
-      .formParam("itmp.address.postCode", "")
-      .formParam("itmp.address.countryName", "")
-      .formParam("itmp.address.countryCode", "")
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("authorityId", _ => "")
+      .formParam("gatewayToken",_ => "")
+      .formParam("redirectionUrl",_ => startPageUrl)
+      .formParam("credentialStrength",_ => "strong")
+      .formParam("confidenceLevel",_ => "50")
+      .formParam("affinityGroup",_ => "Agent")
+      .formParam("usersName",_ => "")
+      .formParam("email",_ => "user@test.com")
+      .formParam("credentialRole",_ => "User")
+      .formParam("nino",_ => "")
+      .formParam("groupIdentifier",_ => "")
+      .formParam("agent.agentId",_ => "")
+      .formParam("agent.agentCode",_ => "")
+      .formParam("agent.agentFriendlyName",_ => "")
+      .formParam("unreadMessageCount",_ => "")
+      .formParam("mdtp.sessionId",_ => "")
+      .formParam("mdtp.deviceId",_ => "")
+      .formParam("presets-dropdown",_ => "SA")
+      .formParam("enrolment[0].name",_ => "")
+      .formParam("enrolment[0].taxIdentifier[0].name",_ => "")
+      .formParam("enrolment[0].taxIdentifier[0].value",_ => "")
+      .formParam("enrolment[0].state",_ => "Activated")
+      .formParam("enrolment[1].name",_ => "")
+      .formParam("enrolment[1].taxIdentifier[0].name",_ => "")
+      .formParam("enrolment[1].taxIdentifier[0].value",_ => "")
+      .formParam("enrolment[1].state",_ => "Activated")
+      .formParam("enrolment[2].name",_ => "")
+      .formParam("enrolment[2].taxIdentifier[0].name",_ => "")
+      .formParam("enrolment[2].taxIdentifier[0].value",_ => "")
+      .formParam("enrolment[2].state",_ => "Activated")
+      .formParam("enrolment[3].name",_ => "")
+      .formParam("enrolment[3].taxIdentifier[0].name",_ => "")
+      .formParam("enrolment[3].taxIdentifier[0].value",_ => "")
+      .formParam("enrolment[3].state",_ => "Activated")
+      .formParam("enrolment[4].name",_ => "HMRC-PODS-ORG")
+      .formParam("enrolment[4].taxIdentifier[0].name",_ => "PSAID")
+      .formParam("enrolment[4].taxIdentifier[0].value",_ => "A2100001")
+      .formParam("enrolment[4].state",_ => "Activated")
+      .formParam("itmp.givenName",_ => "")
+      .formParam("itmp.middleName",_ => "")
+      .formParam("itmp.familyName",_ => "")
+      .formParam("itmp.dateOfBirth",_ => "")
+      .formParam("itmp.address.line1",_ => "")
+      .formParam("itmp.address.line2",_ => "")
+      .formParam("itmp.address.line3",_ => "")
+      .formParam("itmp.address.line4",_ => "")
+      .formParam("itmp.address.line5",_ => "")
+      .formParam("itmp.address.postCode",_ => "")
+      .formParam("itmp.address.countryName",_ => "")
+      .formParam("itmp.address.countryCode",_ => "")
       .check(status.is(303))
   }
 
   def getStartPage: HttpRequestBuilder = {
     http("Get Start Page")
-      .get(startPageUrl)
+      .get(startPageUrl: String)
       .check(status.is(200))
-      .check(saveCsrfToken)
   }
 
   def getMemberDetailsPage: HttpRequestBuilder = {
     http("Get Members Details Page")
-      .get(memberDetailsPageUrl)
+      .get(memberDetailsPageUrl: String)
       .check(status.is(200))
-      .check(saveCsrfToken)
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
   }
 
   def postMemberDetailsPage: HttpRequestBuilder = {
     http("Post to Member-Details Page")
-      .post(memberDetailsPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("firstName", "Pfirstnamel")
-      .formParam("lastName", "Blastnamen")
+      .post(memberDetailsPageUrl: String)
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("firstName",_=> "Pfirstnamel")
+      .formParam("lastName",_=> "Blastnamen")
       .check(status.is(303))
   }
 
@@ -174,16 +175,16 @@ object MPERequests extends HttpConfiguration with ServicesConfiguration {
     http("Get Members DOB Page")
       .get(memberDOBPageUrl)
       .check(status.is(200))
-      .check(saveCsrfToken)
+      .check(css("input[name=csrfToken]", "value").saveAs("csrfToken"))
   }
 
   def postMemberDOBPage: HttpRequestBuilder = {
     http("Post to Member DOB Page")
       .post(memberDOBPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("dateOfBirth.day", "2")
-      .formParam("dateOfBirth.month", "12")
-      .formParam("dateOfBirth.year", "1939")
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("dateOfBirth.day",_ => "2")
+      .formParam("dateOfBirth.month",_ => "12")
+      .formParam("dateOfBirth.year",_ => "1939")
       .check(status.is(303))
   }
 
@@ -191,30 +192,29 @@ object MPERequests extends HttpConfiguration with ServicesConfiguration {
     http("Get Members NINO Page")
       .get(memberNINOPageUrl)
       .check(status.is(200))
-      .check(saveCsrfToken)
   }
 
   def postMemberNINOPage: HttpRequestBuilder = {
     http("Post to Member NINO Page")
       .post(memberNINOPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("nino", testNinoResultsPage)
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("nino",_ => testNinoResultsPage)
       .check(status.is(303))
   }
 
   def postMemberNINONoResultsPage: HttpRequestBuilder = {
     http("Post to Member NINO Page")
       .post(memberNINOPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("nino", testNinoNoResultsPage)
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("nino",_ => testNinoNoResultsPage)
       .check(status.is(303))
   }
 
   def postMemberNINODefaultErrorPage: HttpRequestBuilder = {
     http("Post to Member NINO Page")
       .post(memberNINOPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("nino", testNinoDefaultErrorPage)
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("nino",_ => testNinoDefaultErrorPage)
       .check(status.is(303))
   }
 
@@ -222,14 +222,13 @@ object MPERequests extends HttpConfiguration with ServicesConfiguration {
     http("Get Members PSA Check Ref Page")
       .get(memberPSACheckRefPageUrl)
       .check(status.is(200))
-      .check(saveCsrfToken)
   }
 
   def postMemberPSACheckRefPage: HttpRequestBuilder = {
     http("Post to Member PSA Check Ref Page")
       .post(memberPSACheckRefPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
-      .formParam("psaCheckRef", psaCheckRef)
+      .formParam("csrfToken", _("csrfToken").as[String])
+      .formParam("psaCheckRef",_ => psaCheckRef)
       .check(status.is(303))
   }
 
@@ -242,7 +241,7 @@ object MPERequests extends HttpConfiguration with ServicesConfiguration {
   def postCYAPage: HttpRequestBuilder = {
     http("Post Check Your Answers Page")
       .post(cyaPageUrl)
-      .formParam("csrfToken", "#{csrfToken}")
+      .formParam("csrfToken", _("csrfToken").as[String])
       .check(status.is(303))
   }
 
