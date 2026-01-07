aff065dc4be55079fd94d8b836a7314871e0a465
prode13 <190367850+prode13@users.noreply.github.com>
2026-01-07 14:21:40 +0000

    Removed unused imports

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,6 @@ import sbt._
 object Dependencies {
 
   val compile: Seq[ModuleID] = Seq(
-    "org.playframework" %% "play-ahc-ws-standalone" % "3.0.10",
-    "uk.gov.hmrc" %% "performance-test-runner"   % "6.3.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.20.1",
-    "org.playframework" %% "play-ws-standalone-json" % "3.0.10"
+    "uk.gov.hmrc" %% "performance-test-runner"   % "6.3.0"
   )
 }
